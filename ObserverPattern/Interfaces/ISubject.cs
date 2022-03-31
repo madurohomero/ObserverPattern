1e2c8f414e763b980ab7f8a7f3a650a242955b71
madurohomero <homeromaduro.info@gmail.com>
2022-03-31 10:45:04 -0300

    Adicionar arquivos de projeto.

---
+++ ObserverPattern/Interfaces/ISubject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern.Interfaces
+{
+    public interface ISubject
+    {
+        void Anexar(IObserver observer);
+        void Notificar();
+    }
+}
